c27c2b0e1d44afb6016d45843d8ca5b18cf5edfb
coed95 <codecoed95@gmail.com>
2024-08-13 18:05:11 +0200

    Import each chapter image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './styles.css';
+
 import Majima from './images/majima.jpeg';
 import MajimaFooter from './images/majima-footer.png';
+
+import Chapter1 from './images/chapter-1.png';
+import Chapter2 from './images/chapter-2.png';
+import Chapter3 from './images/chapter-3.png';
+import Chapter4 from './images/chapter-4.png';
+import Chapter5 from './images/chapter-5.png';
+import Chapter6 from './images/chapter-6.png';
+import Chapter7 from './images/chapter-7.png';
+import Chapter8 from './images/chapter-8.png';
+import Chapter9 from './images/chapter-9.png';
+import Chapter10 from './images/chapter-10.png';
+import Chapter11 from './images/chapter-11.png';
+import Chapter12 from './images/chapter-12.png';
+import Chapter13 from './images/chapter-13.png';
+import Chapter14 from './images/chapter-14.png';
+import Chapter15 from './images/chapter-15.png';
+import Chapter16 from './images/chapter-16.png';
+import Chapter17 from './images/chapter-17.png';
+
 
 function setHeaderImage(src, alt) {
     const image = document.querySelector('.header img');
@@ -15,11 +35,30 @@ function setFooterImage(src, alt) {
 }
 
 function setChapterImage(numberOfChapters) {
+    const chapterImages = {
+        1: Chapter1,
+        2: Chapter2,
+        3: Chapter3,
+        4: Chapter4,
+        5: Chapter5,
+        6: Chapter6,
+        7: Chapter7,
+        8: Chapter8,
+        9: Chapter9,
+        10: Chapter10,
+        11: Chapter11,
+        12: Chapter12,
+        13: Chapter13,
+        14: Chapter14,
+        15: Chapter15,
+        16: Chapter16,
+        17: Chapter17
+    };
     const chapterDivs = document.querySelectorAll('.chapter-div');
 
     for (let index = 0; index < numberOfChapters; index++) {
         const image = chapterDivs[index].querySelector('img');
-        image.src = `../src/images/chapter-${index + 1}.png`;
+        image.src = chapterImages[index + 1];
     }
 }
 
